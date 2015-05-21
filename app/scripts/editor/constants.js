1963a32e53ea566f6c314584a48718bc7959e272
David Pronk <git@davidpronk.nl>
2015-05-21 18:17:14 +0200

    constants.js should not be in git

--- app/scripts/editor/constants.js
+++ app/scripts/editor/constants.js
@@ -1,8 +1,8 @@
 angular.module('SE')
 
-.constant('JS_SERVER', 'http://js.vpro.nl')
+.constant('JS_SERVER', 'http://localhost:8081')
 
-.constant('DATA_SERVER', 'http://files.vpro.nl/scenario-editor')
+.constant('DATA_SERVER', 'http://localhost:8010')
 
 .constant('ASSET_ROOT', 'http://files.vpro.nl/scenario-editor/projects/srebrenica/project-assets/')
 
