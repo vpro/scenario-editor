4f468ff3d6a9a0d4911131783e89769d520a9ea9
David Pronk <git@davidpronk.nl>
2015-05-21 18:26:06 +0200

    removed console.log

--- app/scripts/editor/controllers/MainController.js
+++ app/scripts/editor/controllers/MainController.js
@@ -90,7 +90,6 @@ angular.module('SE').controller('MainController', [
 
                 if( assetPath ){
                     this.currentCaller.value = assetPath;
-                    console.log( 'angular.element($(this.currentCaller)): ', angular.element($(this.currentCaller)) );
                     angular.element($(this.currentCaller)).triggerHandler('input');
                 }
 
