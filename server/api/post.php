85b509bb498c7ca20612d61a2c282ef137f6e58c
frenkie <frank@frankbosma.nl>
2015-05-22 18:21:07 +0200

    - fixed doc

--- server/api/post.php
+++ server/api/post.php
@@ -12,7 +12,7 @@ $callback = Util::getRequestVar("callback", "string");
 // respond to preflights
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   // return only the headers and not the content
-  // only allow CORS if we're doing a GET - i.e. no saving for now.
+  // only allow CORS if we're doing a POST
   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
